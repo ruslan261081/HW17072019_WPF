3df25a05e9135296ab6318852dc679e52fc300f6
ruslan261081 <45044693+ruslan261081@users.noreply.github.com>
2019-07-21 22:52:54 +0300

    Add files via upload

---
+++ HW17072019_WPF/HW17072019_WPF/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW17072019_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void Calc()
+        {
+            try
+            {
+                resultTxtBlk.Text = (Int32.Parse(number1Txt.Text) + Int32.Parse(number2Txt.Text)).ToString();
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void sumBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Calc();
+        }
+
+        private void number1Txt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Calc();
+        }
+
+        private void number2Txt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Calc();
+        }
+    }
+}
